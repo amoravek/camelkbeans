31133a0050d0c819f46f0a5feca821c4c21529ed
amoravek <amoravek@gmail.com>
2023-02-24 02:36:38 +0100

    updates

--- src/main/java/cz/trask/camelk/RandomStringsBean.java
+++ src/main/java/cz/trask/camelk/RandomStringsBean.java
@@ -1,16 +1,23 @@
 package cz.trask.camelk;
 
-import java.nio.charset.Charset;
 import java.util.Random;
 
 public class RandomStringsBean {
-	
-	private String generateRandomString(final int length) {
-	    byte[] array = new byte[length];
-	    new Random().nextBytes(array);
-	    String generatedString = new String(array, Charset.forName("UTF-8"));
 
-	    return generatedString;
+	private final String generateRandomString(final int length) {
+
+		int leftLimit = 97; // letter 'a'
+		int rightLimit = 122; // letter 'z'
+
+		Random random = new Random();
+		StringBuilder buffer = new StringBuilder(length);
+		for (int i = 0; i < length; i++) {
+			int randomLimitedInt = leftLimit + (int) (random.nextFloat() * (rightLimit - leftLimit + 1));
+			buffer.append((char) randomLimitedInt);
+		}
+		String generatedString = buffer.toString();
+
+		return generatedString;
 	}
 
 	public String getRandomString(int lenght) {
